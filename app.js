4f3b863a0e9c51fe160ac74b90b75c41a46ebad0
unknown <haib6273@gmail.com>
2022-05-25 08:40:15 +0700

    slider

---
+++ app.js
@@ -0,0 +1,10 @@
+var index = 1;
+changeImage = function () {
+  var imgs = ["./css/img/slider_1.webp", "./css/img/slider_2.webp"];
+  document.getElementById("slider_img").src = imgs[index];
+  index++;
+  if (index == 2) {
+    index = 0;
+  }
+};
+setInterval(changeImage, 2000);
