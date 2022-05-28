4904f73025831ba5fb49fc950008f05e6e339f99
vuchithanh10102002 <93379012+vuchithanh10102002@users.noreply.github.com>
2022-05-29 00:25:37 +0700

    'x'

--- app.js
+++ app.js
@@ -3,38 +3,58 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const imgSlider = $$('.slider_image');
+const imgWrapper = $('.slider-wrapper');
 const iconRightBtn = $('.icon_right');
 const iconLeftBtn = $('.icon_left');
 
 //Chuyen hinh anh content
 var indexImg = 0;
+var positionX = 0;
+var sliderWidth = imgSlider[0].offsetWidth;
 
 function changeImage () {
-  for (var i = 0; i < imgSlider.length; i++) {
-    imgSlider[i].style.display = "none";
+  if (indexImg >= 0 && indexImg < imgSlider.length) {
+    indexImg++;
+    positionX = positionX - sliderWidth;
+    imgWrapper.style = `transform: translate3d(${positionX}px, 0px, 0px)`;
   }
-  indexImg++;
-  if (indexImg > imgSlider.length) {
-    indexImg = 1;
+  if (indexImg == imgSlider.length){
+    indexImg = 0;
+    positionX = 0;
+    imgWrapper.style = `transform: translate3d(0px, 0px, 0px)`;
   }
-  imgSlider[indexImg-1].style.display = "block";
   setTimeout(changeImage, 5000);
 }
 changeImage();
 
 //chuyen hinh anh bang btn
-function nextImg () {
+function nextImg() {
   indexImg++;
-  if (indexImg > imgSlider.length) {
-    indexImg = 1;
+  if (indexImg >= imgSlider.length) {
+    indexImg = imgSlider.length - 1;
+    return;
+  }else {                                                                                          
+    positionX = positionX - sliderWidth;
   }
-  imgSlider[indexImg].style.display = "block";
-  imgSlider[indexImg - 1].style.display = "none";
+  imgWrapper.style = `transform: translate3d(${positionX}px, 0px, 0px)`;
 }
 iconRightBtn.addEventListener("click", () => {
   nextImg();
-})
+});
 
-console.log(iconRightBtn)
-console.log(iconLeftBtn)
+function prevImg() {
+  indexImg--;
+  if (indexImg < 0) {
+    indexImg = 0;
+    return;
+  }
+  positionX = positionX + sliderWidth;
+  imgWrapper.style = `transform: translate3d(${positionX}px, 0px, 0px)`;
+  console.log(indexImg)
+}
+iconLeftBtn.addEventListener("click", () => {
+  prevImg();
+});
+
+
 
