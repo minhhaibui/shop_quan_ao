ea64417102a0cdc52bf78a80f9d2473b352f1589
vuchithanh10102002 <93379012+vuchithanh10102002@users.noreply.github.com>
2022-06-19 01:46:55 +0700

    'giohang'

--- app.js
+++ app.js
@@ -75,3 +75,216 @@ const navsile = () => {
   });
 };
 navsile();
+
+//open-close-Modal
+const buyBtns = document.querySelectorAll(".btn_detail");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".btn-close-modal");
+
+function openModal() {
+  modal.classList.add("open");
+}
+
+function closeModal() {
+  modal.classList.remove("open");
+}
+
+modalClose.addEventListener("click", closeModal);
+
+//click add modal
+buyBtns.forEach(function (buyBtn, index) {
+  buyBtn.addEventListener("click", function (e) {
+    openModal();
+    const buyItem = e.target;
+    const productItem = buyItem.parentElement;
+    const productImg = productItem.querySelector(".front img").src;
+    const productTitle = productItem.querySelector(
+      ".product_card-title"
+    ).textContent;
+    const productPrice = productItem.querySelector(
+      ".product_card-price"
+    ).textContent;
+    addCart(productImg, productTitle, productPrice);
+    deleteCard();
+  });
+});
+
+function addCart(img, title, price) {
+  const headerModal = document.querySelector(".header-modal a");
+  headerModal.innerHTML = `${title}`;
+  const addTr = document.createElement(`tr`);
+  const trHtml = `
+    <tr>
+      <td>
+        <th style="width: 55%; text-align: left;">
+            <div class="img-product-modal">
+                <img src="${img}" alt="" width="140px" height="140px">
+                <div class="add-product">
+                    <h3 class="header-product-add">
+                        <a href="">${title}</a>
+                    </h3>
+                    <div class="size-product-item">
+                        <p class="text-color-product">Size:</p>
+                        <div class="size">M</div>
+                        <div class="size">L</div>
+                        <div class="size">XL</div>
+                    </div>
+                    <div class="color-product">
+                      <p class="text-color-product">Màu sắc:</p>
+                      <div class="color-item">
+                          <img src="${img}" alt="" class="img-color">
+                          <p class="sub-text">Pink</p>
+                      </div>
+  
+                      <div class="color-item">
+                          <img src="${img}" alt="" class="img-color">
+                          <p class="sub-text">Blue</p>
+                      </div>
+                    </div>
+                    <p class="product-close">
+                      <i class="ti-close"></i>
+                      Bỏ sản phẩm
+                    </p>
+                    <p class="product-recently">
+                        <i class="ti-check"></i>
+                        Sản phẩm vừa thêm!
+                    </p>
+                </div>
+            </div>
+        </th>
+      </td>
+  
+      <td>
+        <th style="width: 15%;"><span class="price">${price}
+        </span>
+        <i class="dong-icon">đ</i>
+        </th>
+      </td>
+  
+      <td>
+        <th style="width: 15%;">
+            <div class="btn-change-modal">
+              <input type="number" class="quantity" min="1" max="10" value="1"></input>
+            </div>
+        </th>
+      </td>
+  
+      <td>
+        <th style="width: 15%;"><span class="intoMoney">390.000
+        </span>
+        <i class="dong-icon">đ</i>
+        </th>
+      </td>
+    </tr>
+    `;
+  addTr.innerHTML = trHtml;
+  const cartTable = document.querySelector("tbody");
+  cartTable.append(addTr);
+  totals();
+  changeQuantity();
+  productNumber();
+  totalItem();
+  btnContinue();
+  sizeClick();
+  colorClick();
+}
+
+// total
+function totals() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  var sum = 0;
+  for (var i = 0; i < cartItem.length; i++) {
+    var quantity = cartItem[i].querySelector(".quantity").value;
+    var price = cartItem[i].querySelector(".price").innerHTML;
+    total = Number(quantity) * Number(price) * 1000;
+    sum = sum + total;
+  }
+  const sumMoney = document.querySelector(".sum-money .total-money");
+  sumMoney.innerHTML = sum.toLocaleString("de-DE");
+}
+
+// totalItem
+function totalItem() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  var sum = 0;
+  for (var i = 0; i < cartItem.length; i++) {
+    var quantity = cartItem[i].querySelector(".quantity").value;
+    var price = cartItem[i].querySelector(".price").innerHTML;
+    total = Number(quantity) * Number(price) * 1000;
+    const intoMoney = cartItem[i].querySelector(".intoMoney");
+    intoMoney.innerHTML = total.toLocaleString();
+  }
+}
+
+//delete card
+function deleteCard() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  for (var i = 0; i < cartItem.length; i++) {
+    const deleteBtns = document.querySelectorAll("tr .product-close");
+    deleteBtns[i].addEventListener("click", function (e) {
+      const btn = e.target;
+      const deleteE =
+        btn.parentElement.parentElement.parentElement.parentElement;
+      deleteE.remove();
+      totals();
+      productNumber();
+    });
+  }
+}
+
+//change quantity
+function changeQuantity() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  for (var i = 0; i < cartItem.length; i++) {
+    const quantity1 = cartItem[i].querySelector(".btn-change-modal .quantity");
+
+    quantity1.addEventListener("change", function () {
+      totals();
+      totalItem();
+    });
+  }
+}
+
+function productNumber() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  const productNumber = document.querySelector(".quantity-cart span");
+  productNumber.innerHTML = cartItem.length;
+}
+
+function btnContinue() {
+  const btnContinue = document.querySelector(".btn-continue");
+  const modal = document.querySelector(".modal");
+  btnContinue.addEventListener("click", function () {
+    closeModal();
+  });
+}
+
+//size
+function sizeClick() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  for (var i = 0; i < cartItem.length; i++) {
+    $(".size-product-item .size").click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  }
+}
+
+//color
+function colorClick() {
+  const cartItem = document.querySelectorAll(".cardModal tr");
+  for (var i = 0; i < cartItem.length; i++) {
+    $(".color-product .color-item").click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  }
+}
+
+//cartClick
+function cartClick() {
+  const cartClicks = document.querySelector(".cart-left");
+  cartClicks.addEventListener("click", function () {
+    openModal();
+  });
+}
+
+cartClick();
